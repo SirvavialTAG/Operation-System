e0448f3609e22095b8839040451dc1441aeb189d
Kurennu <kurenu66@gmail.com>
2024-05-28 00:34:00 +1000

    docker done

--- server/index.js
+++ server/index.js
@@ -1,43 +1,26 @@
 const express = require('express');
-const { Pool } = require('pg');
+const Pool = require("pg").Pool;
 const cors = require('cors');
-
 const app = express();
 const port = 5000;
-
+app.use(express.json());
 app.use(cors());
 
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: 'postgres',
   database: 'DataBase1',
   password: 'astro20B',
   port: 5432,
 });
+module.exports = pool;
 
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error acquiring client', err.stack);
-  }
-  client.query('SELECT NOW()', (err, result) => {
-    release();
-    if (err) {
-      return console.error('Error executing query', err.stack);
-    }
-    console.log('Connection to database established at:', result.rows[0].now);
-  });
-});
-
-app.get('/api/goods', async (req, res) => {
+app.get('/goods', async (req, res) => {
   try {
-    const goodsQuery = `
-      SELECT g.product_id, g.product_name, c.category_name, g.price
-      FROM goods g
-      JOIN categories c ON g.category = c.categories_id
-    `;
-    const result = await pool.query(goodsQuery);
-    res.json(result.rows);
-  } catch (err) {
+    const goodsQuery = await pool.query("SELECT * FROM goods");;
+    res.json(goodsQuery.rows);
+  }
+  catch (err) {
     console.error(err);
     res.status(500).send('Server error');
   }
